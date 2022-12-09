f71e767ab5a9da839fe0d563614b1a89cecf227c
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-12-09 03:53:32 +0000

    organised files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import CalendarLogic from './components/Calendar/CalendarLogic';
-import CalendarModal from './components/CalendarModal/CalendarModal';
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
 
-function App() {
-  document.title = 'Calendar';
+function CalendarLogic() {
+  const [date, setDate] = useState(new Date());
+  const test = () => {
+    console.log('test');
+  };
   return (
     <>
-      <CalendarLogic />
-      <CalendarModal />
+      <div className='border-2 border-white rounded-md flex flex-col items-center justify-center text-center p-4'>
+        <Calendar
+          onChange={setDate}
+          value={date}
+          className='p-2 border-2 border-red-900'
+          onClickDay={test}
+        />
+        <p className='border-white border-2 p-2 ' onClick={test}>
+          Here Is Your Selected Date: {date.toDateString()}
+        </p>
+      </div>
     </>
   );
 }
 
-export default App;
+export default CalendarLogic;
--- src/components/CalendarModal/CalendarModal.jsx
+++ src/components/CalendarModal/CalendarModal.jsx
@@ -1,13 +1,11 @@
 import React, { useState } from 'react';
 
-const Modal = ({ message, isOpen, onClose }) => {
+const Modal = () => {
   const [open, setOpen] = useState(false);
-  if (!isOpen) return null;
 
   return (
     <div>
       <div className='border-4 border-red-900 w-96 h-96 flex flex-col justify-end'>
-        {open}
         <button className='border-2 border-white w-20'>X</button>
       </div>
     </div>
