c3935cc6c083665ccb581ba12a2464d16a6125e4
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-12-09 00:03:31 +0000

    new logic

--- src/components/Calendar/CalendarLogic.jsx
+++ src/components/Calendar/CalendarLogic.jsx
@@ -2,12 +2,22 @@ import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 
 function CalendarLogic() {
-  const [value, onChange] = useState(new Date());
-
+  const [date, setDate] = useState(new Date());
+  const test = () => {
+    console.log(date);
+  };
   return (
     <>
-      <div className='border-2 border-white rounded-md flex flex-col items-center justify-center text-center'>
-        <Calendar onChange={onChange} value={value} />
+      <div className='border-2 border-white rounded-md flex flex-col items-center justify-center text-center p-4'>
+        <Calendar
+          onChange={setDate}
+          value={date}
+          className='p-2 border-2 border-red-900'
+          onClickDay={test}
+        />
+        <p className='border-white border-2 p-2 ' onClick={test}>
+          Here Is Your Selected Date: {date.toDateString()}
+        </p>
       </div>
     </>
   );
