7145d1342187c6ef0d83ad64e67944b434f2d55f
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-12-09 03:25:42 +0000

    changed component name

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import CalendarLogic from './components/Calendar/CalendarLogic';
+import CalendarModal from './components/CalendarModal/CalendarModal';
 
 function App() {
   document.title = 'Calendar';
   return (
     <>
       <CalendarLogic />
+      <CalendarModal />
     </>
   );
 }
