4dc20e4ebbfa0e8e70ce18c36f865b67107dfbde
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-12-11 00:04:48 +0000

    added months

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [monthIndex, setMonthIndex] = useState(new Date().getMonth());
+
   const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   const currentDate = new Date();
+  let allMonths = [
+    'January',
+    'Febuary',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  const dec = allMonths[currentDate.getMonth()];
+
   console.log(currentDate);
   return (
     <>
       <div className='border-2 border-white rounded-md flex flex-col items-center justify-center text-center p-4'>
-        {/* <Calendar onChange={setDate} value={date} className onClickDay={test} /> */}
         <div className='w-96 h-96 mx-auto bg-slate-400'>
           <header className='flex justify-evenly m-3'>
-            <div className='bg-red-200 text-lg font-bold'>{'<<'}</div>
-            <div className='bg-red-200 text-lg font-bold'>{'<'}</div>
-            <div className='bg-red-200 text-lg font-bold'>
-              {'December 2022'}
-            </div>
-            <div className='bg-red-200 text-lg font-bold'>{'>'}</div>
-            <div className='bg-red-200 text-lg font-bold'>{'>>'}</div>
+            <button className='bg-red-200 text-lg font-bold'>{'<<'}</button>
+            <button className='bg-red-200 text-lg font-bold'>{'<'}</button>
+            <div className='bg-red-200 text-lg font-bold'></div>
+            <button className='bg-red-200 text-lg font-bold'>{'>'}</button>
+            <button className='bg-red-200 text-lg font-bold'>{'>>'}</button>
           </header>
           {/* MONDAY TO SUN */}
           <div className='flex justify-evenly'>
             {days.map((day) => (
               <div className='m-3'>
                 <div className='font-bold'>{day}</div>
-                <p className='font-bold'>{currentDate.getDate() - 5}</p>
+                <p className='font-bold'>{currentDate.getDate()}</p>
               </div>
             ))}
           </div>
         </div>
-
         {/* <p className='border-white npmborder-2 p-2 ' onClick={test}>
           Here Is Your Selected Date: {date.toDateString()}
         </p> */}
