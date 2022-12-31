5b0dd1b4b3af3aa0f0a1794cc796f9f4f318c130
Haroon Khan <HaroonKhan120704@gmail.com>
2022-12-31 05:48:36 +0000

    added previous jsx here in new file

--- src/components/Months.tsx
+++ src/components/Months.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
-const Months = () => {
-  return <div></div>;
+const Months = ({ monthIndex, setMonthIndex, allMonths }: any) => {
+  return (
+    <header className='flex justify-evenly m-3'>
+      <button className='bg-white text-lg font-bold'>{'<<'}</button>
+      <button
+        className='bg-white text-lg font-bold'
+        onClick={() => {
+          if (monthIndex == 0) return setMonthIndex(11);
+          setMonthIndex(monthIndex - 1);
+        }}
+      >
+        {'<'}
+      </button>
+      <div className='bg-white text-lg font-bold'>{allMonths[monthIndex]}</div>
+      <button
+        className='bg-white text-lg font-bold'
+        onClick={() => {
+          if (monthIndex == 11) return setMonthIndex(0);
+          setMonthIndex(monthIndex + 1);
+        }}
+      >
+        {'>'}
+      </button>
+      <button className='bg-white text-lg font-bold'>{'>>'}</button>
+    </header>
+  );
 };
 
 export default Months;
