dac13c5bf48d82c5530af5b5ced1572e6059885d
Haroon Khan <HaroonKhan120704@gmail.com>
2023-01-05 04:54:38 +0000

    added new styles and testing logic

--- src/components/Dates.tsx
+++ src/components/Dates.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const Dates = ({ calendar }: any) => {
-  const asd = new Date().getDate();
-
+  function test() {
+    calendar;
+  }
   return (
     <>
       <div className='flex justify-evenly flex-col border-2 border-white'>
@@ -15,6 +16,7 @@ const Dates = ({ calendar }: any) => {
                     onClick={() => {
                       let currentDate = day?.getDate();
                       console.log(currentDate);
+                      console.log('testingh');
                     }}
                     className='border-2 border-red-900 ml-8 p-2 text-white w-20'
                   >
@@ -25,10 +27,14 @@ const Dates = ({ calendar }: any) => {
             </div>
           );
         })}
-      </div>
-      <div className='bg-blue-800 border-4 p-5 mt-10 border-white rounded-md'>
-        <h2 className='text-white font-bold'>{`You have selected ${asd}`}</h2>
-        <input className='mt-5' type='text' />
+        <div className='bg-blue-800 border-4 p-5 mt-10 border-white rounded-md'>
+          <h2 className='text-white font-bold text-xl p-3'>{`You have selected ${'asd'}`}</h2>
+          <p className='text-white font-bold'>Add Event Below</p>
+          <input
+            className='mt-5 p-2 rounded-md border-2 border-black '
+            type='text'
+          />
+        </div>
       </div>
     </>
   );
