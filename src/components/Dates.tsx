4e6e035e3d86892d52c83c77401ab187e8943100
Haroon Khan <HaroonKhan120704@gmail.com>
2023-01-13 08:44:25 +0000

    added new logic and changed colors

--- src/components/Dates.tsx
+++ src/components/Dates.tsx
@@ -4,16 +4,17 @@ const Dates = ({ calendar }: any) => {
   const [date, setDate] = useState();
   const [text, setText] = useState('');
 
-  const type = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setText(e.target.value);
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    console.log(text);
   };
 
   return (
     <>
-      <div className='flex justify-evenly flex-col border-2 border-white'>
+      <div className='flex justify-evenly flex-col border-2 border-red-800'>
         {calendar.map((weeks: any[]) => {
           return (
-            <div className='border-2 border-black flex flex-row m-3  '>
+            <div className='flex flex-row m-3  '>
               {weeks.map((day) => {
                 return (
                   <button
@@ -21,7 +22,7 @@ const Dates = ({ calendar }: any) => {
                       let currentDate = day?.getDate();
                       setDate(currentDate);
                     }}
-                    className='border-2 border-red-900 ml-8 p-2 text-white w-20'
+                    className='border-2 border-red-900 ml-8 p-2 text-red-800 w-20'
                   >
                     {day?.getDate()}
                   </button>
@@ -31,22 +32,24 @@ const Dates = ({ calendar }: any) => {
           );
         })}
         <div>
-          <div className='bg-blue-800 border-4 p-5 mt-10 border-white rounded-md flex flex-col items-center'>
-            <h2 className='text-white font-bold text-xl p-3'>{`You have selected day: ${date}`}</h2>
-            <p className='text-white font-bold'>Add Event Below</p>
-            <input
-              className='mt-5  p-2 rounded-md border-2 border-black '
-              type='text'
-              onChange={(e) => type(e)}
-              placeholder='Enter Text Here'
-            />
-            <button className='border-2 border-white rounded-md p-4 text-white font-bold mt-5'>
-              Add Task
-            </button>
+          <div className='bg- border-4 p-5 mt-10 border-red-800 rounded-md flex flex-col items-center'>
+            <h2 className='text-red-800 font-bold text-xl p-3'>{`You have selected day: ${date}`}</h2>
+            <p className='text-red-800 font-bold'>Add Event Below</p>
+            <form onSubmit={handleSubmit}>
+              <input
+                className='mt-5 p-2 rounded-md border-2 text-center border-black '
+                type='text'
+                placeholder='Enter Task Here'
+                onChange={(e) => setText(e.target.value)}
+              />
+              <button className='border-2 border-red-800 rounded-md p-4 text-red-800 font-bold mt-5'>
+                Add Task
+              </button>
+            </form>
           </div>
           <div>
-            <p className='text-white font-bold text-lg'>Current Tasks</p>
-            <p className='text-white mt-5'>{text}</p>
+            <p className='text-red-800 font-bold text-lg'>Current Tasks</p>
+            <p className='text-red-800 mt-5'>{'this is a test'}</p>
           </div>
         </div>
       </div>
