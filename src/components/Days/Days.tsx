37068f101fda9b30a7b6cafadca7fb0fda5c0948
Haroon Khan <HaroonKhan120704@gmail.com>
2023-01-17 00:01:32 +0000

    put files in folder

---
+++ src/components/Days/Days.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Days = ({ days }: any) => {
+  return (
+    <div className='flex justify-evenly'>
+      {days.map((day: any) => (
+        <div className='m-3'>
+          <div className='font-bold text-red-800'>{day}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Days;
