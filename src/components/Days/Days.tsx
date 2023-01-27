81ef21c14f29e53226c0101faa43797b39dfef66
Haroon Khan <HaroonKhan120704@gmail.com>
2023-01-27 00:13:08 +0000

    removed warnings

--- src/components/Days/Days.tsx
+++ src/components/Days/Days.tsx
@@ -2,10 +2,8 @@ const Days = ({ days }: any) => {
   return (
     <div className='flex justify-evenly'>
       {days.map((day: any, index: any) => (
-        <div className='m-3'>
-          <div className='font-bold text-red-800' key={index}>
-            {day}
-          </div>
+        <div className='m-3' key={index}>
+          <div className='font-bold text-red-800'>{day}</div>
         </div>
       ))}
     </div>
